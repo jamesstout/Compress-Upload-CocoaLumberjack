17a177e541585cd7d44667aed1fbfed2e8abce32
James Stout <stoutyhk@gmail.com>
2021-03-18 17:53:25 +0800

    test1

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .target(
             name: "Compress-Upload-CocoaLumberjack",
             dependencies: ["CocoaLumberjack"],
-            path: "Classes",
+            path: "/Classes",
             publicHeadersPath: "."),
     ]
 )
